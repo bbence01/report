ba9100beb83e85b67c6500b1279f6bd0a5ee892e
hallgato <hallgato@LABOR215-15>
2023-11-28 16:27:12 +0100

    Add project files.

---
+++ report/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using report.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace report.Controllers
+{
+
+
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public IActionResult Index()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/tickets", "tickets.txt");
+            var lines = System.IO.File.ReadAllLines(path);
+
+            var tickets = lines.Select(line => new Ticket { Content = line }).ToList();
+
+            return View(tickets);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ report/Models/Ticket.cs
@@ -0,0 +1,8 @@
+﻿namespace report.Models
+{
+
+    public class Ticket
+    {
+        public string Content { get; set; }
+    }
+}
